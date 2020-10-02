49bbf9190cdc7ac796af33ac48e891d2b9b83537
emmeiwhite <emmeiwhite@gmail.com>
2020-10-02 14:19:30 +0530

    Front End Functionality covered

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
-const cafeForm = document.querySelector(".add-cafe-form");
+const cafeForm = document.querySelector("#cafe-form");
+const cafeList = document.querySelector(".cafe-lists");
 
+// 1) Form Submit
 cafeForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const cafeName = cafeForm.cafeName.value;
   const cafeCity = cafeForm.cafeCity.value;
-  console.log(cafeName);
-  console.log(cafeCity);
+
+  const htmlElement = `<li class="cafe-info"> 
+      <div class="cafe-details">
+        <h4>${cafeName}</h4>
+        <p>${cafeCity}</p>
+      </div>
+
+      <span class="delete-cafe">X</span>
+  </li>`;
+
+  cafeList.innerHTML += htmlElement;
+  cafeForm.reset();
+});
+
+// 2) Remove the Item using Event Bubbling
+cafeList.addEventListener("click", (e) => {
+  console.log("Target Element is :");
+  console.log(e.target.className);
+  if (e.target.className === "delete-cafe") {
+    e.target.parentElement.remove();
+  } else {
+    console.log("Other Child Element of Parent");
+  }
 });
